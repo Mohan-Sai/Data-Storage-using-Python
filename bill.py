6fbc92780b51b2112234423044bca93e43d669e3
Mohan-Sai <33582360+Mohan-Sai@users.noreply.github.com>
2019-04-29 14:30:01 +0530

    Add files via upload

---
+++ bill.py
@@ -0,0 +1,45 @@
+import sqlite3
+conn=sqlite3.connect('bill.db')
+c=conn.cursor()
+bill={"Gulshan":9896621213,"Mohan":9160706262}
+cust_name=input("enter the name")
+cust_details={}
+
+
+#c.execute("""CREATE TABLE customers(
+#            Name varchar,
+#            Phone_Number integer
+#       )""")
+
+
+c.execute("INSERT INTO customers VALUES('Sai',9160786262)")
+#database using dictionary
+def billing(name):
+    if name in bill:
+        print("Existing User")
+    else:
+        cust_num=input("Enter your phone number")
+        bill[name]=cust_num
+    print(bill)
+
+
+
+#database using sqlite db
+def billing1(name):
+    c.execute("SELECT * FROM customers WHERE Name=(?)",(cust_name,))
+    data=c.fetchall()
+    print(data)
+    print(len(data))
+    if (len(data)!=0):
+        print("Fuck off")
+    else:
+        cust_num=input("Enter your phone number")
+        bill[name]=cust_num
+        
+        c.execute("INSERT INTO customers VALUES(:Name,:Phone_Number)",{'Name':cust_name,'Phone_Number':cust_num})
+        c.execute("SELECT * FROM customers")
+        print(c.fetchall())
+        conn.commit()
+        conn.close()
+
+billing1(cust_name)
